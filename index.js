535ff6b0d62d610b13c739bfe22bca501160bba8
Deepak Venkatesan <Deepakbakyalakshmi1997@gmail.com>
2023-11-12 23:25:41 +0530

    new cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { studentrouter } from "./routers/studentRouter.js";
 import { Mentorrouter } from "./routers/mentorRouter.js";
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/students", studentrouter);
